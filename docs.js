43256c5662e0e6137fc898fdb5023bbc4184c3d6
Anton Bachin <antonbachin@yahoo.com>
2021-04-04 13:14:34 +0300

    Docs site

---
+++ docs.js
@@ -0,0 +1,44 @@
+console.log("foo");
+
+function current_section() {
+  var threshold = window.innerHeight / 2;
+  var sections = document.querySelectorAll("h2");
+  var latest;
+
+  for (var counter = 0; counter < sections.length; ++counter) {
+    var section = sections[counter];
+
+    var offset = section.getBoundingClientRect().top;
+    if (offset < threshold)
+      latest = section;
+  }
+
+  var links = document.querySelectorAll("nav.odoc-toc li");
+  for (counter = 0; counter < links.length; ++counter)
+    links[counter].classList.remove("current-section");
+
+  if (latest === undefined)
+    return;
+
+  for (counter = 0; counter < links.length; ++counter) {
+    if (links[counter].innerText === latest.innerText)
+      links[counter].classList.add("current-section");
+  }
+};
+
+function sidebar_position() {
+  var toc = document.querySelector(".odoc-toc");
+  var header = document.querySelector("header");
+  if (window.scrollY >= header.offsetHeight)
+    toc.classList.add("fixed");
+  else {
+    toc.classList.remove("fixed");
+  }
+};
+
+function scroll() {
+  current_section();
+  sidebar_position();
+};
+
+window.onscroll = scroll;
